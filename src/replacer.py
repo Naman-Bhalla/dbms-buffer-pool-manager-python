40c289e807f5cfa1bc70cb95e14735c8640570c6
Naman Bhalla <namanbhalla1998@gmail.com>
2018-04-05 23:07:02 +0530

    Added abstract class for Replacer

---
+++ src/replacer.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Replacer(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def insert(self, frame):
+        pass
+
+    @abstractmethod
+    def victim(self, frame):
+        pass
+
+    @abstractmethod
+    def erase(self, frame):
+        pass
+
+    @abstractmethod
+    def __len__(self):
+        pass
