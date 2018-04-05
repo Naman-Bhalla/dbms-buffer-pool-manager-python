4ffb806d23da531a7f66eb9396af8384a0e34918
Naman Bhalla <namanbhalla1998@gmail.com>
2018-04-06 01:12:32 +0530

    Add STUB class for Disk Manager operations.

---
+++ src/disk_manager.py
@@ -0,0 +1,16 @@
+class DiskManager:
+
+    def __init__(self, page_size):
+        self.page_size = page_size
+
+    def readPage(self, page_id, data):
+        pass
+
+    def writePage(self, page_id, data):
+        pass
+
+    def allocatePage(self):
+        pass
+
+    def deAllocatePage(self):
+        pass
